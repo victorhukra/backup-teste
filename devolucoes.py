75a67b4eeac05d80d616b669b1f65d0544415c79
victormaximiano <128063057+victorhukra@users.noreply.github.com>
2025-12-08 15:53:36 -0300

    Fix message for empty book collection

--- devolucoes.py
+++ devolucoes.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def devolucoesEmprestimo(conj,acervo): #aqui temos um problema com as datas, como cada empréstimo tem um limites de 7 dias, testando o código nunca vai dar uma devolução atrasada
     if len(acervo)==0:
-        print('\nNão há livros no acervo')
+        print('\nNão há livros no acervo.')
         return
     
     devolucao=str(input('\nDigite o nome do livro a ser devolvido: '))
@@ -28,12 +28,13 @@ def devolucoesEmprestimo(conj,acervo): #aqui temos um problema com as datas, com
                             dif=(dataDevolucao-dataPrevista).days
 
                             if dif>0:
-                                print(f'\nO livro está atrasado há {dif} dias\nUma taxa de {dif*0.5} será aplicada')
+                                print(f'\nO livro está atrasado há {dif} dias\nUma taxa de {dif*0.5} será aplicada.')
                             else:
-                                print(f'\nO livro foi devolvido dentro do prazo\nNenhuma taxa adicional será aplicada')
+                                print(f'\nO livro foi devolvido dentro do prazo\nNenhuma taxa adicional será aplicada.')
 
                             return
                         
-                    print('\nNenhum empréstimo do livro foi encontrado')
+                    print('\nNenhum empréstimo do livro foi encontrado.')
         
+
                     
